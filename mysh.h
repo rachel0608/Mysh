cfd0dadc438b7e09536dc9be50857f5074c53cd9
jvrieger <57420053+jvrieger@users.noreply.github.com>
2024-02-18 16:32:37 -0500

    Create mysh.h
    this header file holds includes, defines, globals, and function prototypes

---
+++ mysh.h
@@ -0,0 +1,42 @@
+//mysh.h
+
+#ifndef MYSH_H
+#define MYSH_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <readline/readline.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <pthread.h>
+
+#define MAX_ARGS 51 // max number of arguments, including NULL
+#define DELIM " \t\n&" // delimiters to tokenize user input command
+#define EXIT "exit" // built-in command: exit
+#define STATUS_TERMINATE    2
+#define STATUS_SUSPEND  1
+#define STATUS_RESUME   0
+#define TRUE 1
+#define FALSE 0
+
+volatile sig_atomic_t sigchld_received = 0; // flag for sig_chld_handler to update
+volatile sig_atomic_t sigtstp_received = 0; // flag for sig_tstp_handler to update
+volatile pid_t pid_to_update; //pid of Job to update
+pid_t foreground_pid = -1;  // Initialize to an invalid PID, set in execute_command
+
+char **parse_command(char *command_line, bool *background);
+void execute_command(char **args, bool *background);
+int check_blank_input(char *command_line);
+int is_built_in(char *args);
+void free_args(char **args);
+
+void update_joblist(pid_t pid_to_update, int status_to_update); //update JOBLL based on what signals were caught: finds job in LL with pid "pid_to_update", sets status to "status_to_update"
+void block_sigchld(int block); //function to block or unblock SIGCHLD- signal masking
+void sig_chld_handler(int sig, siginfo_t *info, void *ucontext); //handles SIGCHLD
+void sig_tstp_handler(int signo); //handles SIGTSTP
+void setup_sighandlers(); //sets up signal handlers with sigaction()
+
+#endif
