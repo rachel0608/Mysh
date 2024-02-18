682b0ddd1ff7ddbf5d10292cb82f32b8dafb22d6
rachel0608 <khanhchi121@gmail.com>
2024-02-18 17:42:31 -0500

    Feature: support multiple & and ;

--- mysh.c
+++ mysh.c
@@ -13,98 +13,114 @@
 #include <readline/readline.h>
 #include <stdbool.h>
 
-#define MAX_ARGS 51 // max number of arguments, including NULL
-#define DELIM " \t\n&" // delimiters to tokenize user input command
+#define MAX_ARGS 5 // max number of arguments, including NULL
+#define DELIM " \t\n" // delimiters to tokenize user input command
 #define EXIT "exit" // built-in command: exit
 
-char **parse_command(char *command_line, bool *background);
-void execute_command(char **args, bool *background);
-int check_blank_input(char *command_line);
+typedef struct {
+    char *cmdline;
+    bool bg;
+} Command;
+
+char **parse_command(Command *command);
+void execute_command(char **args, bool bg);
+int check_blank_input(char *command_line); 
 int is_built_in(char *args);
 void free_args(char **args);
+char *remove_space(char *str);
+bool check_syntax(char* input);
+char **tokenize(char *input, char *delim);
+Command **parse_to_structs(char *command_list);
+void free_command_list(Command **commands);
 
 int main() {
+    bool exit = false;
     // continuosly prompt user to enter command
     while (true) {
         // print prompt
-        char *command_line = readline("$ ");
+        char *input = readline("$ ");
 
-        if (check_blank_input(command_line)) {
-            free(command_line);
+        // if blank input or has syntax error related to delim, go to the next loop
+        if (check_blank_input(input) || !check_syntax(input)) {
+            free(input);
+            continue;
+        } 
+
+        // tokenize with ; to get a list of sequential commands to be executed
+        char **command_list = tokenize(input, ";");
+        if (command_list == NULL) {
+            printf("Error tokenizing with ;\n");
+            free(input);
             continue;
         }
 
-        // parse command line into argument
-        bool background = false;
-        char **args = parse_command(command_line, &background);
-        free(command_line);
-        if (args == NULL) {
-            printf("Error parsing command.\n");
-            continue;
-        }
-
-        // check whether argument is built in command, then execute in fg
-        if (is_built_in(args[0])) {
-            // exit
-            if (strcmp(EXIT, args[0]) == 0) {
-                printf("Goodbye!\n");
-                free_args(args);
+        // for each sequential command, find the processes
+        for (int i = 0; command_list[i] != NULL; i++) {
+            Command **commands = parse_to_structs(command_list[i]);
+            if (commands == NULL) {
+                printf("Error tokenizing with &\n");
+                free(command_list[i]);
+                continue;
+            }
+            
+            // make this a func
+            // for each process, parse into args then execute
+            for (int j = 0; commands[j] != NULL; j++) {
+                if (check_blank_input(commands[j]->cmdline)) {
+                    free(commands[j]->cmdline);
+                    free(commands[j]);
+                    continue;
+                } else {
+                    // parse into args
+                    char **args = parse_command(commands[j]);
+                    free(commands[j]->cmdline);
+                    free(commands[j]);
+                    if (args == NULL) {
+                        printf("Error parsing command.\n");
+                        continue;
+                    }
+                    // execute
+                    if (is_built_in(args[0])) {
+                        // exit
+                        if (strcmp(EXIT, args[0]) == 0) {
+                            printf("Goodbye!\n");
+                            free_args(args);
+                            exit = true;
+                            break;
+                        }
+                    } else {
+                        // execute command
+                        execute_command(args, commands[j]->bg);
+                        free_args(args);
+                    }
+                }
+            }
+            free_command_list(commands);
+            if (exit) {
                 break;
             }
-        } else {
-            // execute command
-            execute_command(args, &background);
-            free_args(args);
+        }
+
+        free_args(command_list);
+        free(input);
+
+        if (exit) {
+            break;
         }
     }
+
     return 0;
 }
 
 /**
  * @brief Parse the user input into the argument
  *
- * @param command_line Input from user
+ * @param command Input from user
  * @return An array of args on success, NULL on error
  */
-char **parse_command(char *command_line, bool *background) {
-    // check if user enter only delim
-    bool is_input_delim = strspn(command_line, DELIM) == strlen(command_line);
-    if (is_input_delim) {
-        return NULL;
-    }
-
-    char **args = malloc(MAX_ARGS * sizeof(char *)); // a list of args to be returned
-    if (args == NULL) {
-        perror("malloc");
-        return NULL;
-    }
-
-    // check if user enter &
-    size_t len = strlen(command_line);
-    *background = (command_line[len - 1] == '&');
-
+char **parse_command(Command *command) {
     // tokenize the input
-    char *token = strtok(command_line, DELIM);
-    int i = 0;
-    while (token != NULL && i < MAX_ARGS - 1) {
-        args[i] = strdup(token); 
-        if (args[i] == NULL) {
-            perror("strdup");
-            free_args(args);
-            return NULL;
-        }
-        token = strtok(NULL, DELIM);
-        i++;
-    }    
-
-    if (i == MAX_ARGS - 1 && token != NULL) {
-        printf("Error: Too many arguments\n");
-        free_args(args);
-        return NULL;
-    }
-
-    args[i] = NULL; 
-    return args;
+    return tokenize(command->cmdline, DELIM);
 }
 
 /**
@@ -162,7 +178,7 @@ void free_args(char **args) {
  * @param args The arguments for the command
  * @param background Flag indicating whether the command should run in the background
  */
-void execute_command(char **args, bool *background) {
+void execute_command(char **args, bool background) {
     pid_t pid = fork();
     if (pid < 0) {
         perror("fork");
@@ -175,7 +191,7 @@ void execute_command(char **args, bool *background) {
             exit(EXIT_FAILURE);
         } 
     } else {
-        if (!*background) {
+        if (!background) {
             int status;
             waitpid(pid, &status, 0);
         } else {
@@ -184,4 +200,159 @@ void execute_command(char **args, bool *background) {
             // add child to the job queue
         }
     }
+}
+
+bool check_syntax(char* input) {
+    // check if the input starts with a delim
+    bool start_with_delim = (strchr("&;", input[0]) != NULL);
+    if (start_with_delim) {
+        printf("Error: Syntax error near unexpected token '&' or ';'\n");
+        return false;
+    }
+
+    // check for "&&", "&;", ";&", or ";;"
+    char *input_without_space = remove_space(input);
+    if (input_without_space == NULL) {
+        return false;
+    }
+
+    char* error_combinations[] = {"&&", "&;", ";&", ";;"};
+    int num_combinations = sizeof(error_combinations) / sizeof(error_combinations[0]);
+
+    for (int i = 0; i < num_combinations; i++) {
+        if (strstr(input_without_space, error_combinations[i]) != NULL) {
+            printf("Error: Syntax error near unexpected token '&' or ';'\n");
+            free(input_without_space);
+            return false; 
+        }
+    }
+    free(input_without_space);
+    return true; 
+}
+
+char *remove_space(char *str) {
+    int len = strlen(str);
+    char* result = (char *) malloc((len + 1) * sizeof(char)); 
+    if (result == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return NULL;
+    }
+
+    int j = 0;
+    for (int i = 0; i < len; i++) {
+        if (!isspace(str[i])) { // check if the character is not a whitespace
+            result[j++] = str[i];
+        }
+    }
+    result[j] = '\0'; // null-terminate 
+
+    return result;
+}
+
+char **tokenize(char *input, char *delim) {
+    char **tokens = (char **)malloc(MAX_ARGS * sizeof(char *));
+    if (tokens == NULL) {
+        perror("malloc");
+        return NULL;
+    }
+
+    char *input_copy = strdup(input); // preserve the input
+    if (input_copy == NULL) {
+        perror("strdup");
+        return NULL;
+    }
+
+    int token_count = 0;
+    char *token = strtok(input_copy, delim);
+    while (token != NULL && token_count < MAX_ARGS - 1) {
+        tokens[token_count] = strdup(token);
+        if (tokens[token_count] == NULL) {
+            perror("strdup");
+            free_args(tokens);
+            exit(EXIT_FAILURE);
+        }
+        token = strtok(NULL, delim);
+        token_count++;
+    }
+
+    if (token_count == MAX_ARGS - 1 && token != NULL) {
+        printf("Error: Too many arguments\n");
+        free_args(tokens);
+        return NULL;
+    }
+
+    tokens[token_count] = NULL; // null-terminate the array of tokens
+    free(input_copy); 
+    return tokens;
+}
+
+Command **parse_to_structs(char *command_list) {
+    // check if the command at the end of command_list has & in it
+    // if it does, the last token is also a bg process
+    bool flag = false;
+    char *str = remove_space(command_list);
+    size_t len = strlen(str);
+    if (str[len - 1] == '&') {
+        flag = true;
+    } 
+    free(str);
+
+    // tokenize by '&' to find the bg processes and storing them in Command structs
+    char **tokens = tokenize(command_list, "&");
+    if (tokens == NULL) {
+        printf("Error tokenizing with &\n");
+        return NULL;
+    }
+
+    int count = 0; // the number of bg processes
+    for (int i = 0; tokens[i] != NULL; i++) {
+        count++;
+    }
+
+    Command **commands = (Command **) malloc(((count + 1) * sizeof(Command))); // null terminated
+    if (commands == NULL) {
+        perror("Memory allocation failed");
+        free_args(tokens);
+        return NULL;
+    }
+
+    // populate the list of commands
+    for (int i = 0; i < count; i++) {
+        commands[i] = malloc(sizeof(Command));
+        if (commands[i] == NULL) {
+            perror("Memory allocation failed");
+            free_command_list(commands); 
+            free_args(tokens);
+            return NULL;
+        }
+
+        if (i == count - 1) {
+            commands[i]->cmdline = strdup(tokens[i]);
+            commands[i]->bg = flag; 
+        } else {
+            commands[i]->cmdline = strdup(tokens[i]);
+            commands[i]->bg = true; 
+        }
+    }
+    commands[count] = NULL; // null-terminate the array
+
+    // print the list of commands
+    for (int i = 0; tokens[i] != NULL; i++) {
+        printf("Debug: Command %d: %s (Background: %s)\n", i + 1, commands[i]->cmdline, commands[i]->bg ? "true" : "false");
+    }
+
+    // free memory
+    free_args(tokens);
+
+    return commands;
+}
+
+void free_command_list(Command **commands) {
+    // Free each command list and its elements
+    for (int i = 0; commands[i] != NULL; i++) {
+        free(commands[i]->cmdline);
+        free(commands[i]);
+    }
+    // Free the array of command lists
+    free(commands);
 }
