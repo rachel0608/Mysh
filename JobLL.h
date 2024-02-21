df43944d2647b28d3a6c6af493b534f14ed0363f
jvrieger <57420053+jvrieger@users.noreply.github.com>
2024-02-20 22:47:59 -0500

    Update JobLL.h

--- JobLL.h
+++ JobLL.h
@@ -3,8 +3,15 @@
 #ifndef JOB_LL_H
 #define JOB_LL_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 #include "node.h"
 #include "Job.h"
+
+#define EMPTY 0
+#define ONE 1
 
 // Define data structures and prototypes
 typedef struct {
@@ -16,6 +23,8 @@ typedef struct {
 JobLL *new_list(); //Creates a new empty list
 int size(const JobLL *l); //returns size of LL
 int empty(const JobLL *l); //returns if LL is empty (1=empty, 0=notempty)
+int find_job(const JobLL *l, pid_t pid); //returns the index of the job with a certain pid (counting from end of LL)
+Job *remove_nth_job(JobLL *l, int n); //removes nth node and returns Job in node
 Job *remove_first_job(JobLL *l); //removes head and returns Job
 void add_job(JobLL *l, Job *j); //add Job at end of LL
 void print_jobs(const JobLL *l); //prints all jobs in LL
