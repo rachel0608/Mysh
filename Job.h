c6ea95d09cc213c346fcab01bfde84c02233b43f
jvrieger <57420053+jvrieger@users.noreply.github.com>
2024-02-20 22:47:06 -0500

    Update Job.h

--- Job.h
+++ Job.h
@@ -3,9 +3,13 @@
 #ifndef JOB_H
 #define JOB_H
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
 
 typedef struct Job {
+  int jid; //job id
     pid_t pid; //process ID
     char *command; //command being executed
     int status; // Running (1) or Suspended (0)
@@ -20,5 +24,6 @@ int get_status(const Job *j);
 char *job_to_string(const Job *j);
 
 void set_job_status(Job *j, int status); //suspended = 0, running = 1
+void free_job(Job *job);
 
 #endif
