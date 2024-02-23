5b0223e2b124007883f4b2988d6eb5460b606e05
jvrieger <57420053+jvrieger@users.noreply.github.com>
2024-02-22 22:48:57 -0500

    Update Job.h

--- Job.h
+++ Job.h
@@ -6,14 +6,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 #include <sys/types.h>
 
 typedef struct Job {
-  int jid; //job id
+    int jid; //job id
     pid_t pid; //process ID
     char *command; //command being executed
-    int status; // Running (1) or Suspended (0)
-} Job;
+    int status; // Running (0) or Suspended (1) or Terminated/Done (2)
+    struct termios* setting; //terminal settings info
+    //pointer to last job backgrounded/foregrounded
+    volatile sig_atomic_t handle_flag; // flag for sig_chld_handler to update
+}   Job;
 
 Job *new_job(pid_t pid, char *command); //given process ID and command, create Job (init as running by default) and return pointer
 
@@ -24,6 +28,7 @@ int get_status(const Job *j);
 char *job_to_string(const Job *j);
 
 void set_job_status(Job *j, int status); //suspended = 0, running = 1
-void free_job(Job *job);
+void free_job(Job *job); //free memory allocated for Job struct
+void handle_flag_true(Job *job, int boolean); //set handle flag to boolean (true or false)
 
 #endif
